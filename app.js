e65409f85c7a3319bd71f1ba87112e11eca66115
Syed M. Taha <s.m.taha10@gmail.com>
2016-05-18 18:59:13 +0500

    Single page app look and feel

---
+++ app.js
@@ -0,0 +1,51 @@
+ (function () {
+
+     var editor = document.getElementById("editor")
+     var selectedTabName = "tab01";
+     var selectedDOMElement = document.getElementById(selectedTabName);
+     console.log(selectedTabName);
+     var htmlEditor = document.getElementById("html-editor");
+     var cssEditor = document.getElementById("css-editor");
+     var jsEditor = document.getElementById("js-editor");
+     function whatClicked(evt) {
+         if(selectedTabName === "tab01"){
+             selectedDOMElement.className = "tab";
+             //htmlEditor.className = "";
+             //cssEditor.className = "hidden"
+         }
+         else if(selectedTabName === "tab02" || selectedTabName === "tab03"){
+             selectedDOMElement.className = "tab tab-other";
+         }
+
+
+         selectedTabName = evt.target.id;
+         selectedDOMElement = document.getElementById(selectedTabName);
+         console.log(selectedTabName, selectedDOMElement);
+         if(selectedTabName === "tab01"){
+             selectedDOMElement.className = "tab highlight";
+             document.getElementById("tab02").className = "tab tab-other up"
+             htmlEditor.className = "";
+             cssEditor.className = "hidden";
+             jsEditor.className = "hidden";
+         }
+         else if(selectedTabName === "tab02" || selectedTabName === "tab03"){
+             selectedDOMElement.className = "tab tab-other highlight";
+             if(selectedTabName === "tab02"){
+                 cssEditor.className = "";
+                 jsEditor.className = "hidden";
+             }
+             else {
+                 cssEditor.className = "hidden";
+                 jsEditor.className = "";
+             }
+             htmlEditor.className = "hidden";
+
+
+         }
+     }
+
+     editor.addEventListener("click", whatClicked, false);
+
+ })();
+
+
