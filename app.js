3e4458cfaac16164c8226ab492175e28deecf6bb
Syed M. Taha <s.m.taha10@gmail.com>
2016-05-18 19:39:29 +0500

    At a stable point

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
  (function () {
 
-     var editor = document.getElementById("editor")
+     //         Changing Editor in tab click
+
+     var editor = document.getElementById("editor");
+
      var selectedTabName = "tab01";
+
      var selectedDOMElement = document.getElementById(selectedTabName);
+
      console.log(selectedTabName);
+
      var htmlEditor = document.getElementById("html-editor");
      var cssEditor = document.getElementById("css-editor");
      var jsEditor = document.getElementById("js-editor");
+
      function whatClicked(evt) {
+
          if(selectedTabName === "tab01"){
              selectedDOMElement.className = "tab";
              //htmlEditor.className = "";
@@ -17,13 +25,15 @@
              selectedDOMElement.className = "tab tab-other";
          }
 
-
          selectedTabName = evt.target.id;
+
          selectedDOMElement = document.getElementById(selectedTabName);
+
          console.log(selectedTabName, selectedDOMElement);
+
          if(selectedTabName === "tab01"){
              selectedDOMElement.className = "tab highlight";
-             document.getElementById("tab02").className = "tab tab-other up"
+             document.getElementById("tab02").className = "tab tab-other up";
              htmlEditor.className = "";
              cssEditor.className = "hidden";
              jsEditor.className = "hidden";
@@ -46,6 +56,20 @@
 
      editor.addEventListener("click", whatClicked, false);
 
+     //         Changing Editor on tab click
+
+     //         Opening Menu on menu icon click
+
+     var nav = document.getElementsByTagName("nav").item(0);
+     var menu;
+     function openMenu(evt){
+        menu = evt.target.id;
+        if(menu == "menu"){
+            console.log(nav);
+        }
+     }
+
+     nav.addEventListener("click", openMenu, false);
  })();
 
 
